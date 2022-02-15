b40795864ca16cd26a1854cd02a17a96956fa565
Jang Oo Bin <jwb449730@naver.com>
2022-02-15 17:12:14 +0900

    리액트를 다루는 기술 8장 / react hooks

--- src/Average.js
+++ src/Average.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 const getAverage = (numbers) => {
   console.log("평균값 계산중 ...");
@@ -10,17 +10,21 @@ const getAverage = (numbers) => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
-  const onChange = (e) => {
+  const onChange = useCallback((e) => {
+    // useCallback() :첫번째 파라미터에는 생성하고 싶은 함수, 두번째 파라미터에는 배열
     setNumber(e.target.value);
-  };
-  const onInsert = (e) => {
-    const nextList = list.concat(parseInt(number));
-    setList(nextList);
-    setNumber("");
-  };
+  }, []);
+  const onInsert = useCallback(
+    (e) => {
+      const nextList = list.concat(parseInt(number));
+      setList(nextList);
+      setNumber("");
+    },
+    [number, list]
+  );
 
   const avg = useMemo(() => getAverage(list), [list]);
-  // useMemo() : 렌더링하는 과정에서 특정 값이 바뀌었을 때만 연산을 실행
+  // useMemo() : 렌더링하는 과정에서 특정 값(list)이 바뀌었을 때만 연산을 실행
   // 함수형 컴포넌트 내부에서 발생하는 연산을 최적화
   return (
     <div>
@@ -39,3 +43,8 @@ const Average = () => {
 };
 
 export default Average;
+
+/*
+숫자, 문자열, 객체처럼 일반 값을 재사용하려면 -> useMemo
+함수를 재사용하려면 -> useCallback
+*/
